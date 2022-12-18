63de20295d740b529be3ef1279478f66295b504b
GreeshmaSreeParimi <115046800+GreeshmaSreeParimi@users.noreply.github.com>
2022-12-18 03:09:39 +0000

    Commiting intial changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,51 @@ import Modals from './components/Modals';
 import Nav from './components/Nav';
 import "./App.scss";
 
+
 function App() {
-  const [cast, setCast] = useState([]);
-  let [memberInfo, setMemberInfo] = useState(null);
-
-  async function fetchCast() {
-    const response = await fetch('cast.json');
-    setCast(await response.json());
-  }
-
-  useEffect(() => {
-    fetchCast();
-  });
-
+  const name = "Star Gazers";
   return (
-    <>
-      <Nav cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
       <div className="container">
-        <hgroup>
-          <img src="images/group.svg" alt="StarGazers Group" />
-          <h1>Meet the Stargazers</h1>
-          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <ListCast cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
-          {memberInfo && <Modals member={memberInfo} handleChange={(info) => { setMemberInfo(cast[info]) }} handleClose={() => { setMemberInfo(null) }} />}
-        </hgroup>
+        <article>
+          <hgroup>
+            <img src="images/group.svg" alt="StarGazers Group" />
+            <h1>Meet the <i style={{color : 'steelblue'}}>{name}</i></h1>
+            <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+            <button className="outline" onClick= {()=> alert("Hi staar gazers !!!")}>Click Me</button>
+          </hgroup>
+        </article>
       </div>
-    </>
   )
 }
+
+// function App() {
+//   const [cast, setCast] = useState([]);
+//   let [memberInfo, setMemberInfo] = useState(null);
+
+//   async function fetchCast() {
+//     const response = await fetch('cast.json');
+//     setCast(await response.json());
+//   }
+
+//   useEffect(() => {
+//     fetchCast();
+//   });
+
+//   return (
+//     <>
+//       <Nav cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
+//       <div className="container">
+//         <article>
+//           <hgroup>
+//             <img src="images/group.svg" alt="StarGazers Group" />
+//             <h1>Meet the Stargazers</h1>
+//             <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+//             <ListCast cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
+//             {memberInfo && <Modals member={memberInfo} handleChange={(info) => { setMemberInfo(cast[info]) }} handleClose={() => { setMemberInfo(null) }} />}
+//           </hgroup>
+//         </article>
+//       </div>
+//     </>
+//   )
+// }
 export default App
